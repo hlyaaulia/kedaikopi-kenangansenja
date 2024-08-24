7fa0cce74a103d90b283babd3e50fa7b629b5a0c
hlyaaulia <hlyaaulia0910@gmail.com>
2024-08-24 19:59:11 +0700

    membuat website kedai kopi kenangan senja

---
+++ js/script.js
@@ -0,0 +1,9 @@
+//toggle class active//
+
+const navbarNav = document.querySelector
+('.navbar-nav');
+//ketika humburger-menu di klik 
+document.querySelector('#humburger-menu'). 
+onclick=()=>{
+    navbarNav.classList.toggle('active');
+};
